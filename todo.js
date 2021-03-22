776464f7bb1578bd7d760fa356fb8f6ff112168d
Max16 <max16@naver.com>
2021-03-22 16:53:37 +0900

    #3.5 Making a To Do List part Two

--- todo.js
+++ todo.js
@@ -2,17 +2,31 @@ const toDoForm = document.querySelector(".js-toDoForm"),
   toDoInput = toDoForm.querySelector("input"),
   toDoList = document.querySelector(".js-toDolist");
 
-const TODOS_LS = "toDOs";
+const TODOS_LS = "toDos";
+
+const toDos = [];
+
+function saveToDos() {
+  localStorage.setItem(TODOS_LS, JSON.stringify(toDos)); // localStorage 에서는 모든 data를 string 형태로 저장 -> 객체를 string 형태로 변환해야 함
+}
 
 function paintToDo(text) {
   const li = document.createElement("li");
   const delBtn = document.createElement("button");
-  delBtn.innerText = "❌";
   const span = document.createElement("span");
+  const newId = toDos.length + 1;
+  delBtn.innerText = "❌";
   span.innerText = text;
   li.appendChild(delBtn);
   li.appendChild(span);
+  li.id = newId; // li id
   toDoList.appendChild(li);
+  toDoObj = {
+    text,
+    id: newId,
+  };
+  toDos.push(toDoObj);
+  saveToDos();
 }
 
 function handleSubmit(event) {
@@ -23,8 +37,12 @@ function handleSubmit(event) {
 }
 
 const loadToDos = () => {
-  const toDos = localStorage.getItem(TODOS_LS);
-  if (toDos !== null) {
+  const loadedToDos = localStorage.getItem(TODOS_LS);
+  if (loadedToDos !== null) {
+    const parsedToDos = JSON.parse(loadedToDos); // Stringify 로 만든 string 을 object 형태로 다시 변환
+    parsedToDos.forEach(function (toDo) {
+      paintToDo(toDo.text);
+    });
   }
 };
 
